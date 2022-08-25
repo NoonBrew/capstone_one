da78e0fb26be96e199484aa20a94af58f1c99c9e
Nate Owens <n.owens.dev01@gmail.com>
2022-08-25 18:41:09 -0500

    Updated Birthday function to handle int/string

--- WeekOne/birthdayMessage.py
+++ WeekOne/birthdayMessage.py
@@ -1,16 +1,21 @@
 from datetime import datetime
 
-def GreetingMessage():
-    print('Please Enter your name: ')
-    name = input()
-    print('Greetings, {}, has {} letters!'.format(name.capitalize(), len(name)))
-    print('{}, what month is your birthday?'.format(name.capitalize()))
-    birthdayMonth = input()
+def main():
+    GreetingMessage()
 
-    if int(birthdayMonth) == datetime.now().month or birthdayMonth.capitalize() == datetime.now().strftime('%B'):
+def GreetingMessage():
+    name = input('Please Enter your name: ')
+    print('Greetings, {}, has {} letters!'.format(name.capitalize(), len(name)))
+    birthdayMonth = input('{}, what month is your birthday? '.format(name.capitalize()))
+
+    numberBirthday = 0
+    if birthdayMonth.isnumeric():
+        numberBirthday = int(birthdayMonth)
+        
+    if numberBirthday == datetime.now().month or birthdayMonth.capitalize() == datetime.now().strftime('%B'):
         print('It is your birthday month!!')
     else:
         print('It is not your birthday Month!')
 
-GreetingMessage()
 
+main()
